6bb3fe82af942f1c4e164c86567a1a269f3a5dae
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-15 13:50:55 +0600

    donw with three functions to get candidates votes, get the status and get remaining time

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,16 @@ const App = () => {
 
   //get candidates
   async function getCandidates() {
-    
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstacne = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    const candidatesList = await contractInstacne.getAllVotesOfCandiates();
+    console.log(candidatesList);
   }
 
   //get current status
